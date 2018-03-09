7bc497c991f8ba8448fec0fe0050e404ec52f2d7
Justin Gatlin <jgatlin@agoramedia.com>
2018-03-09 17:15:22 -0500

    First check in

---
+++ __tests__/app/components/CartItem-test.js
@@ -0,0 +1,9 @@
+jest.dontMock('../../../app/components/CartItem.js');
+
+describe("the cart item module", function () {
+    describe("the name display of the item", function () {
+        it("should display the name of the item", function () {
+            var CartItem = require('../../../app/components/CartItem.js');
+        });
+    });
+});
